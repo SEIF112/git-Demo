5da3ed2383ea8bbc19384f5e0f17d0a476410e72
TEUB023 <louis.teubler@bertelsmann.de>
2026-09-16 10:45:53 +0200

    New_Weapon issue previously pushed from a wrong folder, this is the right commit

--- ghDemo/Service/BattleService.cs
+++ ghDemo/Service/BattleService.cs
@@ -9,12 +9,14 @@ namespace ghDemo.Service
 {
     internal class BattleService
     {
-        public void PrintBattle(Hero hero, Enemy enemy)
+        public void PrintBattle(Weapon weapon, Hero hero, Enemy enemy)
         {
             Console.WriteLine($"Hero: {hero.Name}, Health: {hero.Health}");
             Console.WriteLine($"Enemy: {enemy.Name}, Health: {enemy.Health}");
 
             Console.WriteLine($"hero: {hero.Name} fights {enemy.Name}!");
+            Console.WriteLine($"hero: {hero.Name} attacks {enemy.Name} with {weapon.Name}")
+            Console.WriteLine($"{enemy.health} is reduced by {weapon.Damage}")
         }
     }
 }
